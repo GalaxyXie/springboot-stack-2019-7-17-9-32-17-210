3a714b3a5f2ae8b669c5a641fbbd09e33da569d2
GalaxyXie <771472141@qq.com>
2019-07-17 21:22:30 +0800

    [test]implement test should_return_a_criminal_case_when_find_criminal_case_by_Id

--- src/main/java/com/tw/apistackbase/Repository/CriminalCaseRepository.java
+++ src/main/java/com/tw/apistackbase/Repository/CriminalCaseRepository.java
@@ -3,5 +3,9 @@ package com.tw.apistackbase.Repository;
 import com.tw.apistackbase.model.CriminalCase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CriminalCaseRepository extends JpaRepository<CriminalCase,Integer> {
+    CriminalCase findCriminalCaseById(int id);
+    List<CriminalCase> findAllByDateOrderByDateDesc();
 }
--- src/test/java/com/tw/apistackbase/CriminalCaseTest.java
+++ src/test/java/com/tw/apistackbase/CriminalCaseTest.java
@@ -2,13 +2,18 @@ package com.tw.apistackbase;
 
 import com.tw.apistackbase.Repository.CriminalCaseRepository;
 import com.tw.apistackbase.model.CriminalCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -18,10 +23,19 @@ public class CriminalCaseTest {
     CriminalCaseRepository criminalCaseRepository;
 
     @Test
-    public void should_return_criminal_case_when_add_criminal_case(){
-        CriminalCase criminalCase=new CriminalCase("AWomanHasBeenMurder",Long.valueOf("1970010123230404"));
+    public void should_return_List_of_criminal_case_size_when_add_criminal_case(){
+        CriminalCase criminalCase=new CriminalCase("AWomanHasBeenMurdered",Long.valueOf("1970010123230404"));
         criminalCaseRepository.save(criminalCase);
         int size=criminalCaseRepository.findAll().size();
         Assertions.assertEquals(1,size);
+    }
+    @Test
+    public void should_return_a_criminal_case_when_find_criminal_case_by_Id(){
+        CriminalCase case1=new CriminalCase("JerryKilledJaylon",Long.valueOf("1970010123230000"));
+        CriminalCase case2=new CriminalCase("JerryKilledSean",Long.valueOf("1970010123230000"));
+        criminalCaseRepository.save(case1);
+        criminalCaseRepository.save(case2);
+        CriminalCase criminalCase=criminalCaseRepository.findCriminalCaseById(1);
+        Assertions.assertEquals("JerryKilledJaylon",criminalCase.getName());
     }
 }
