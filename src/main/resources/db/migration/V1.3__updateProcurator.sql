d5919fa50ff9506b64eeb5ed22c8a22e35b6b7ed
GalaxyXie <771472141@qq.com>
2019-07-18 17:24:58 +0800

    [feat:]finish insert columns to procurator

---
+++ src/main/resources/db/migration/V1.3__updateProcurator.sql
@@ -0,0 +1,3 @@
+insert into procurator(id,procuratorate_name,sex,age,birthday,IDnumber,experience) values (1,'张三','男',18,20011020,001,0);
+insert into procurator(id,procuratorate_name,sex,age,birthday,IDnumber,experience) values (2,'李四','女',28,19821020,002,10);
+insert into procurator(id,procuratorate_name,sex,age,birthday,IDnumber,experience) values (3,'王五','男',58,19721020,003,15);
