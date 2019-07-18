2a04e68ab3283d5b7943d6685fc08b0687ad0cac
GalaxyXie <771472141@qq.com>
2019-07-18 17:39:03 +0800

    [feat:]finish update info of 'lisi'

---
+++ src/main/resources/db/migration/V1.4__updateProcurator.sql
@@ -0,0 +1,4 @@
+insert into procurator(id,procuratorate_name,sex,age,birthday,IDnumber,experience) values (1,'张三','男',18,20011020,001,1);
+insert into procurator(id,procuratorate_name,sex,age,birthday,IDnumber,experience) values (2,'李四','女',28,19821020,002,10);
+insert into procurator(id,procuratorate_name,sex,age,birthday,IDnumber,experience) values (3,'王五','男',58,19721020,003,15);
+update procurator set age   =37 where procuratorate_name='李四';
